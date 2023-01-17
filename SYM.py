c009903352d3ea5016e4d0e44591ae40c364a495
papatil <papatil@ncsu.edu>
2023-01-17 01:43:33 -0500

    Got Code Running

--- SYM.py
+++ SYM.py
@@ -1,26 +1,26 @@
 import math 
 class SYM:
     def __init__(self):
-        i=0 #remove this
-    def new(i):
-        i.n   = 0
-        i.has = {}
-        i.most, i.mode = 0, None
+        self.n   = 0
+        self.has = {}
+        self.most, self.mode = 0, None
 
-    def add(i,x): 
+    def add(self, x): 
         if x != "?":
-            i.n = i.n + 1 
-            i.has[x] = 1 + (i.has[x] or 0)
-            if i.has[x] > i.most:
-                i.most,i.mode = i.has[x], x 
+            self.n = self.n + 1 
+            if x not in self.has:
+                self.has[x] = 0
+            self.has[x] += 1
+            if self.has[x] > self.most:
+                self.most,self.mode = self.has[x], x
 
-    def mid(i,x):
-        return i.mode 
+    def mid(self):
+        return self.mode 
 
-    def div(i,x, fun,e): 
-        def fun(p):
-            return p*math.log(p,2)
-        e=0; 
-        for _,n in enumerate(i.has):
-            e = e + fun(n/i.n) 
+    def fun(p):
+        return p*math.log(p,2)
+
+    def div(self, fun = fun,e = 0): 
+        for _,n in self.has.items():
+            e = e + fun(n/self.n) 
         return -e 
--- examples.py
+++ examples.py
@@ -3,37 +3,8 @@ import NUM
 import helpers
 import strings
 import lists
-import globals
+import globalVariables
 import os
-
-
-#Main function to be called
-def main(options, help, funs, k, saved, fails):
-    global Seed
-    saved = {}
-    fails = 0
-    for k,v in enumerate(cli(settings(help))):
-        options[k] = v
-        [k] = v
-  
-    if options.help:
-        print(help) 
-    else:
-        for what, fun in enumerate(funs):
-            if options.go == "all" or what == options.go:
-                for k, v in enumerate(saved):
-                    options[k] = v
-                    Seed = options.seed
-                    if funs[what]() == False:
-                        fails += 1
-                        print("❌ fail:",what) 
-                    else: 
-                        print("✅ pass:",what)
-        for k,v in enumerate(_ENV):
-            if not b4[k]:
-               print( f"#W ?%s %s",k,type(v))
-        os.exit(fails) 
-
 
 # Examples Added to the CLI
 examples_added = {}
@@ -45,7 +16,7 @@ def add_example(key, str, fun):
     Adds an example that is runnable from the command line and updates the help menu
     '''
     examples_added[key] = fun
-    globals.help +=  f"  -g  {key}\t{str}\n"
+    globalVariables.help +=  f"  -g  {key}\t{str}\n"
 
 # How to use? First define a function, then add to the examples.
 # def eg_function_0:
@@ -55,22 +26,24 @@ def add_example(key, str, fun):
 
 # Add an example that shows settings
 def eg_function_1():
-    return strings.oo(globals.the)
+    return strings.oo(globalVariables.the)
 
 add_example("the", "show settings", eg_function_1)
 
+global Seed
 
 #Add an example that deals with 'random' numbers
 def eg_function_2():
-    global Seed
-    num1,num2 = NUM(), NUM()
+    
+    num1 = NUM.NUM()
+    num2 = NUM.NUM()
 
-    Seed=globals.the.seed
-    for i in range(1, 10e3):
+    helpers.Seed=globalVariables.the.get("seed")
+    for i in range(1, int(10e3)):
         num1.add( helpers.rand(0,1) )
 
-    Seed=globals.the.seed
-    for i in range(1, 10e3):
+    helpers.Seed=globalVariables.the.get("seed")
+    for i in range(1, int(10e3)):
         num2.add( helpers.rand(0,1) )
 
     m1,m2 = helpers.rnd(num1.mid(),10), helpers.rnd(num2.mid(),10)
@@ -81,7 +54,7 @@ add_example("rand", "generate, reset, regenerate same", eg_function_2)
 
 #Add an example that deals with symbols
 def eg_function_3():
-    sym=SYM()
+    sym=SYM.SYM()
     for x in ["a","a","a","a","b","b","c"]:
         sym.add(x)
     return "a"==sym.mid() and 1.379 == helpers.rnd(sym.div())
@@ -90,12 +63,10 @@ add_example("sym","check syms", eg_function_3)
 
 
 def eg_function_4():
-    num=NUM()
+    num=NUM.NUM()
     for x in [1,1,1,1,2,2,3]:
         num.add(x)
     return 11/7 == num.mid() and 0.787 == helpers.rnd(num.div()) 
   
 add_example("num", "check nums", eg_function_4 )
-
-# main(the, help, examples_added)
 
