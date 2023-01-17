37e6e14f79de041e4a2a84fd14c06aaa92fad9a8
papatil <papatil@ncsu.edu>
2023-01-17 18:40:56 -0500

    Fixed test file

--- HW1/test/test_hw1.py
+++ HW1/test/test_hw1.py
@@ -3,5 +3,5 @@ sys.path.insert(0, '/Users/pradeeppatil/Documents/Classes/Spring 2023/CSC 591 Au
 import run, globalVariables, examples
 
 passed, failed = run.main(globalVariables.the, globalVariables.help, examples.examples_added)
-print("Number passing -> " + str(passed))
-print("Number failing -> " + str(failed))
+print("Number of tests passing -> " + str(passed))
+print("Number of tests failing -> " + str(failed))
