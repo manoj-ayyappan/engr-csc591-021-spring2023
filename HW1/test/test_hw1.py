70cf8bbdff805edc115a4387b839babca4ccc82a
papatil <papatil@ncsu.edu>
2023-01-17 19:25:16 -0500

    github actions

--- HW1/test/test_hw1.py
+++ HW1/test/test_hw1.py
@@ -1,5 +1,7 @@
-import sys
-sys.path.insert(0, '/Users/pradeeppatil/Documents/Classes/Spring 2023/CSC 591 Automated Software Engineering/workspace/engr-csc591-021-spring2023/HW1/src/')
+import sys, os
+print( "----------> " + str(sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'src'))))
+sys.path.append(sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'src')))
+# sys.path.insert(0, '/Users/pradeeppatil/Documents/Classes/Spring 2023/CSC 591 Automated Software Engineering/workspace/engr-csc591-021-spring2023/HW1/src/')
 import run, globalVariables, examples
 
 passed, failed = run.main(globalVariables.the, globalVariables.help, examples.examples_added)
