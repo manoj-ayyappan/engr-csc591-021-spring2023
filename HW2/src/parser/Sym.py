2cc5c485b8b1fdcd5d6e86097e434506c6e796c0
Pradeep <pmap101@gmail.com>
2023-01-24 18:25:04 -0500

    Sym class Complete

--- HW2/src/parser/Sym.py
+++ HW2/src/parser/Sym.py
@@ -1,6 +1,35 @@
-class Sym(object):
-    pass
+import math
 
+
+
+class Sym(object):
+    def __init__(self, at = 0, txt = ""):
+        self.at, self.txt = at , txt 
+        self.n   = 0
+        self.has = {}
+        self.most, self.mode = 0, None
+
+    def add(self, x):
+        if x != "?": 
+            self.n += 1 
+            self.has[x] = 1 + (self.has[x] if self.has[x] else 0)
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x 
+
+    def mid(self):
+        return self.mode 
+    
+    def entropy(p):
+        return p * math.log(p,2) 
+
+    def div(self):
+        e=0; 
+        for _,n in self.has.items():
+            e = e + self.entropy(n/self.n)
+        return -e
+
+    def rnd(self,x):
+        return x 
 
 
 
