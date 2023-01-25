bbb07409d3ed43f2bfd05f6fba176fcea2df4f38
rupintalwar <rupintalwar2@gmail.com>
2023-01-24 19:26:48 -0500

    added Data class

--- HW2/src/lists.py
+++ HW2/src/lists.py
@@ -1,3 +1,12 @@
 def push(t, x):
   t.append(x) 
   return x 
+
+def kap(t, fun) # map function `fun`(k,v) over list (skip nil results) 
+    u = {}
+    for k,v in t.items():
+        v, k = fun(k, v)
+        if k is None:
+            k = len(u) + 1
+        u[k] = v
+        return u
--- HW2/src/parser/Data.py
+++ HW2/src/parser/Data.py
@@ -1,6 +1,37 @@
+from .. import strings
+from .. import lists
+from . import Row
+
+
+# Store many rows, summarized into columns
 class Data(object):
-    pass
+    def __init__(self, src={}):
+        self.rows, self.cols = {}, None
+        if type(src) == "string":
+            # load from a csv file on disk
+            strings.csv(src,self.add)
+        else:
+           # load from a list
+           map(src, self.add)
+    
+    def add(self, t):
+        if self.cols:
+            # true if we have already seen the column names
+            t = t.cells and t or Row(t) # ensure is a ROW, reusing old rows in the are passed in
+            # t =ROW(t.cells and t.cells or t) # make a new ROW
+            lists.push(self.rows, t) # add new data to "i.rows"
+            self.cols.add(t)  # update the summary information in "ic.ols"
+        else:
+           self.cols=COLS(t)  #  here, we create "i.cols" from the first row
 
+    def clone(self, init = {}): # return a DATA with same structure as `ii. 
+        data = Data({self.cols.names})
+        map(init, lambda x: data.add(x))
+        return data
 
+    def stats(self, what, cols, nPlaces): # reports mid or div of cols (defaults to i.cols.y)
+        def fun(k, col):
+            return col.rnd(getattr(col)[what or "mid"](col), nPlaces), col.txt 
+        return lists.kap(cols or self.cols.y, fun)
 
 
