b69734ffb1edfa8e16fa9428ca31be0d3173150a
rupintalwar <rupintalwar2@gmail.com>
2023-01-30 18:25:19 -0500

    add misc

--- HW3/src/misc.py
+++ HW3/src/misc.py
@@ -1,3 +1,11 @@
-def show():
-    # Not Implemented
-    pass
+import strings
+
+def show(node, what, cols, nPlaces, lvl=0): #--> nil; prints the tree generated from `DATA:tree`.
+    if node:
+        print(("| ").rep(lvl) + len(node.data.rows) + "  ")
+        if not node.left or lvl==0:
+            print(strings.o(node.data.stats("mid",node.data.cols.y,nPlaces)))
+        else:
+            print("")
+        show(node.left, what,cols, nPlaces, lvl+1)
+        show(node.right, what,cols,nPlaces, lvl+1)
