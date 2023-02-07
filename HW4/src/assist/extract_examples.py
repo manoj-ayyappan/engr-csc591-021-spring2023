5b22e0f1b6b9e8b29a1045f6c0a62ddb65cb88e2
rupintalwar <rupintalwar2@gmail.com>
2023-02-06 19:48:43 -0500

    created script for extracting examples

---
+++ HW4/src/assist/extract_examples.py
@@ -0,0 +1,71 @@
+#extracts examples from the lua file and prints out layout code 
+
+#What it should end up as:
+#def eg_function_12():
+#   ...
+#def add_all_examples():
+#    add_example("myOwnTest", "MyOwnTest", eg_function_12 )
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+import re
+import glob
+
+
+necessary_text = """
+# Examples Added to the CLI
+examples_added = {}
+
+# Register an example
+def add_example(key, str, fun):
+    global examples_added
+    '''
+    Adds an example that is runnable from the command line and updates the help menu
+    '''
+    examples_added[key] = fun
+    d.help +=  f"  -g  {key}\t{str}\n"
+
+# How to use? First define a function, then add to the examples.
+# def eg_function_0:
+#   return the.some.missing.nested.field
+# eg("crash","show crashing behavior", eg_function_1)
+"""
+
+lua_files = glob.glob('*.lua')
+
+
+def create_output(examples):
+    i = 0
+    print(necessary_text)
+    for example in examples:
+        name, description, code = example
+        #Create a function def
+        print(f"def eg_function_{i}():\n")
+        print(code)
+        i += 1
+
+    print("\n\ndef add_all_examples():\n")
+    i = 0
+    for example in examples:
+        name, description, code = example
+        print(f"\tadd_example({name}, {description}, eg_function_{i})")
+        i += 1
+
+if True:
+    filename = "C:\\Users\\rupin\\source\\repos\\engr-csc591-021-spring2023\\HW4\\src\\assist\\grid.lua"
+#for filename in lua_files:
+    with open(filename, "r") as f:
+        s = f.read()
+
+    examples = []
+    i = 0
+    for option in re.findall("eg\(([^\,]+),([^\,]+),((.|\n)*?end\s*\)\n)", s):
+        # Use capture groups from the regex to get option name and default value
+        name, description, code, useless = option
+        if i != 0:
+            examples.append((name, description, code))
+        i += 1
+    create_output(examples)
--- HW4/src/examples.py
+++ HW4/src/examples.py
@@ -19,99 +19,96 @@ def add_example(key, str, fun):
     Adds an example that is runnable from the command line and updates the help menu
     '''
     examples_added[key] = fun
-    d.help +=  f"  -g  {key}\t{str}\n"
+    d.help +=  f"  -g  {key}    {str}
+"
 
 # How to use? First define a function, then add to the examples.
 # def eg_function_0:
 #   return the.some.missing.nested.field
 # eg("crash","show crashing behavior", eg_function_1)
 
-# Add an example that shows settings
-def eg_function_1():
-    return strings.oo(d.the)
+def eg_function_0():
 
-#Add an example that deals with symbols
+function() oo(the) end)
+
+def eg_function_1():
+
+ function(     t1,t2)
+  t1={a=1,b={c=2,d={3}}}
+  t2=copy(t1)
+  t2.b.d[1]=10000
+  print("b4",o(t1),"\nafter",o(t2))  end)
+
 def eg_function_2():
-    sym = Sym.Sym()
-    for x in ["a","a","a","a","b","b","c"]:
-        sym.add(x)
-    return "a" == sym.mid() and 1.379 == numerics.rnd(sym.div())
+
+ function()
+  local sym=SYM()
+  for _,x in pairs{"a","a","a","a","b","b","c"} do sym:add(x) end
+  return "a"==sym:mid() and 1.379 == rnd(sym:div())end)
 
 def eg_function_3():
-    num = Num.Num()
-    for x in [1,1,1,1,2,2,3]:
-        num.add(x)
-    return 11/7 == num.mid() and 0.787 == numerics.rnd(num.div()) 
 
+ function()
+  local num=NUM()
+  for _,x in pairs{1,1,1,1,2,2,3} do num:add(x) end
+  return 11/7 == num:mid() and 0.787 == rnd(num:div()) end )
 
-## Not Used
 def eg_function_4():
-    global n 
-    n = 0
-    def fun(t):
-        global n
-        n += len(t)
-    strings.csv(d.the.get("file"), fun )
-    return n == 8 * 399
 
+ function(    t)
+  t=repCols( dofile(the.file).cols )
+  map(t.cols.all,oo)
+  map(t.rows,oo)
+end)
 
 def eg_function_5():
-    data = Data.Data(d.the.get("file"))
-    return len(data.rows) == 398 and data.cols.y[0].w == -1 and data.cols.x[0].at == 0 and len(data.cols.x) == 4
+
+ function(    t)
+  show(repCols( dofile(the.file).cols ):cluster())
+end)
 
 def eg_function_6():
-    data = Data.Data(d.the.get("file"))
-    for k,cols in {"y":data.cols.y, "x":data.cols.x}.items():
-        print(k,"\tmid\t", strings.o(data.stats("mid",cols,2 )))
-        print("", "\tdiv\t", strings.o(data.stats("div",cols,2)))
 
+ function(    t,rows)
+  t=dofile(the.file)
+  rows = repRows(t, transpose(t.cols))
+  map(rows.cols.all,oo)
+  map(rows.rows,oo)
+end)
 
 def eg_function_7():
-    data1=Data.Data(d.the.get("file"))
-    data2=data1.clone(data1.rows)
-    return len(data1.rows) == len(data2.rows) and \
-        data1.cols.y[0].w == data2.cols.y[0].w and \
-        data1.cols.x[0].at == data2.cols.x[0].at and  \
-        len(data1.cols.x) == len(data2.cols.x)
+
+ function(    t,rows)
+  t=dofile(the.file)
+  rows = repRows(t, transpose(t.cols))
+  show(rows:cluster())
+end)
 
 def eg_function_8():
-    data=Data.Data(d.the.get("file"))
-    print(0,0,strings.o(data.rows[0].cells))
-    for n,t in data.around(data.rows[0]).items():
-        if (n+1) % 50 == 0:
-           print(n+1, numerics.rnd(t.get("dist"),2) , strings.o(t.get("row").cells))
+
+ function(    t,rows)
+  t=dofile(the.file)
+  rows = repRows(t, transpose(t.cols))
+  rows:cluster()
+  repPlace(rows)
+end)
 
 def eg_function_9():
-    data=Data.Data(d.the.get("file"))
-    left,right,A,B,mid,c = data.half() 
-    print(len(left),len(right),len(data.rows))
-    print(strings.o(A.cells),c)
-    print(strings.o(mid.cells)) 
-    print(strings.o(B.cells))
 
-def eg_function_10():
-    data=Data.Data(d.the.get("file"))
-    misc.show(data.cluster(),"mid",data.cols.y,1)
+ function()
+    repgrid(the.file) end)
 
-def eg_function_11():
-    data=Data.Data(d.the.get("file"))
-    misc.show(data.sway(),"mid",data.cols.y,1)
 
-def eg_function_12():
-    data1=Data.Data(d.the.get("file"))
-    data2 = data1.clone(data1.rows)
-    # print(str(data2.rows[0].cells))
 
 def add_all_examples():
-    add_example("myOwnTest", "MyOwnTest", eg_function_12 )
-    add_example("the", "show settings", eg_function_1)
-    add_example("sym","check syms", eg_function_2)
-    add_example("num", "check nums", eg_function_3 )
-    #add_example("csv", "read from csv", eg_function_4 ) #Not Used
-    add_example("data","read DATA csv", eg_function_5 )
-    #add_example("stats","stats from DATA", eg_function_6 ) #Not Used
-    add_example("clone", "duplicate structure", eg_function_7 )
-    add_example("around", "sorting nearest neighbors", eg_function_8 )
-    add_example( "half", "1-level bi-clustering", eg_function_9 )
-    add_example("cluster", "N-level bi-clustering", eg_function_10 ) 
-    add_example("optimize", "semi-supervised optimization", eg_function_11 )
+
+        add_example("the", "show settings", eg_function_0)
+        add_example("copy", "check copy", eg_function_1)
+        add_example("sym", "check syms", eg_function_2)
+        add_example("num",  "check nums", eg_function_3)
+        add_example("repcols", "checking repcols", eg_function_4)
+        add_example("synonyms", "checking repcols cluster", eg_function_5)
+        add_example("reprows", "checking reprows", eg_function_6)
+        add_example("prototypes", "checking reprows cluster", eg_function_7)
+        add_example("position", "where's wally", eg_function_8)
+        add_example("every", "the whole enchilada", eg_function_9)
