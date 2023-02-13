25642a170fb24a1b5012c51ec2dfc661b83ca2ef
rupintalwar <rupintalwar2@gmail.com>
2023-02-13 18:38:21 -0500

    start examples

--- src/HW5/examples.py
+++ src/HW5/examples.py
@@ -2,12 +2,13 @@ import strings
 import numerics
 import misc
 import lists
+import query
+import discretization
 import globalVars as d
 
 import Num
 import Sym
 import Data
-import lists
 
 
 
@@ -29,124 +30,126 @@ def add_example(key, str, fun):
 # eg("crash","show crashing behavior", eg_function_1)
 
 def eg_function_0():
+    t=[]
+    u=[]
 
- function(     t,u)
-  Seed=1; t={}; for i=1,1000 do push(t,rint(100)) end
-  Seed=1; u={}; for i=1,1000 do push(u,rint(100)) end
-  for k,v in pairs(t) do assert(v==u[k]) end end)
+    d.Seed=1
+    for i in range(1,1000):
+       t.append(numerics.rint(100))
+
+    d.Seed=1
+    for i in range(1,1000):
+       u.append(numerics.rint(100))
+
+    for k,v in enumerate(t):
+       assert(v==u[k])
 
 def eg_function_1():
+  d.the.Max = 32
+  num1 = Num.Num()
+  for i in range(1,10000):
+    lists.add(num1,i)
 
- function(     num1)
-  the.Max = 32
-  num1 = NUM()
-  for i=1,10000 do add(num1,i) end
-  oo(has(num1)) end)
+  strings.oo(query.has(num1))
 
 def eg_function_2():
-
- function(     num1,num2)
-  num1,num2 = NUM(), NUM()
-  for i=1,10000 do add(num1, rand()) end
-  for i=1,10000 do add(num2, rand()^2) end
-  print(1,rnd(mid(num1)), rnd(div(num1)))
-  print(2,rnd(mid(num2)), rnd(div(num2)))
-  return .5 == rnd(mid(num1)) and mid(num1)> mid(num2) end)
+  num1,num2 = Num.Num(), Num.Num()
+  for i in range(1,10000):
+      lists.add(num1, numerics.rand())
+  for i in range(1,10000): 
+      lists.add(num2, numerics.rand()**2)
+  print(1,numerics.rnd(query.mid(num1)), numerics.rnd(query.div(num1)))
+  print(2,numerics.rnd(query.mid(num2)), numerics.rnd(query.div(num2)))
+  return .5 == numerics.rnd(query.mid(num1)) and numerics.mid(num1) > query.mid(num2)
 
 def eg_function_3():
-
- function(    sym)
-  sym=adds(SYM(), {"a","a","a","a","b","b","c"})
-  print (mid(sym), rnd(div(sym)))
-  return 1.38 == rnd(div(sym)) end)
+  sym=lists.adds(Sym.Sym(), {"a","a","a","a","b","b","c"})
+  print (query.mid(sym), numerics.rnd(query.div(sym)))
+  return 1.38 == numerics.rnd(query.div(sym))
 
 def eg_function_4():
+  n=0
+  def fun(t):
+      n += len(t)
 
- function(     n)
-  n=0; csv(the.file, function(t) n=n+#t end)
-  return 3192 == n end)
+  strings.csv(d.the.file, fun)
+  return 3192 == n
 
 def eg_function_5():
-
- function(    data,col)
-  data=DATA.read(the.file)
+  data=Data.Data(d.the.file)
   col=data.cols.x[1]
-  print(col.lo,col.hi, mid(col),div(col))
-  oo(stats(data)) end)
+  print(col.lo,col.hi, query.mid(col), query.div(col))
+  strings.oo(data.stats())
 
 def eg_function_6():
-
-function(    data1,data2)
-  data1=DATA.read(the.file)
-  data2=DATA.clone(data1,data1.rows)
-  oo(stats(data1))
-  oo(stats(data2)) end)
+  data1=Data.Data(d.the.file)
+  data2=data1.clone(data1.rows)
+  strings.oo(data1.stats())
+  strings.oo(data2.stats())
 
 def eg_function_7():
-
- function(   t1,t2,t3)
-  assert(false == cliffsDelta( {8,7,6,2,5,8,7,3},{8,7,6,2,5,8,7,3}),"1")
-  assert(true  == cliffsDelta( {8,7,6,2,5,8,7,3}, {9,9,7,8,10,9,6}),"2")
+  assert(False == cliffsDelta( {8,7,6,2,5,8,7,3},{8,7,6,2,5,8,7,3}),"1")
+  assert(True  == cliffsDelta( {8,7,6,2,5,8,7,3}, {9,9,7,8,10,9,6}),"2")
   t1,t2={},{}
-  for i=1,1000 do push(t1,rand()) end --rand()/10) end
-  for i=1,1000 do push(t2,rand()^.5) end --rand()*10) end
-  assert(false == cliffsDelta(t1,t1),"3")
-  assert(true  == cliffsDelta(t1,t2),"4")
-  local diff,j=false,1.0
-  while not diff  do
-    t3=map(t1,function(x) return x*j end)
+  for i in range(1,1000):
+    lists.push(t1,numerics.rand())
+  for i in range(1,1000):
+      lists.push(t2,numerics.rand()**.5)
+  assert(False == cliffsDelta(t1,t1),"3")
+  assert(True  == cliffsDelta(t1,t2),"4")
+  diff,j = False,1.0
+
+  def fun(x):
+      return x*j
+
+  while not diff:
+    t3=lists.map(t1,fun)
 
 def eg_function_8():
-
- function(    data,num)
-  data = DATA.read(the.file)
-  num  = NUM()
-  for _,row in pairs(data.rows) do
-    add(num,dist(data, row, data.rows[1])) end
-  oo{lo=num.lo, hi=num.hi, mid=rnd(mid(num)), div=rnd(div(num))} end)
+  data = Data.Data(d.the.file)
+  num  = Num.Num()
+  for _,row in enumerate(data.rows):
+    num.add(data.dist(row, data.rows[1]))
+  strings.oo({"lo":num.lo, 
+              "hi":num.hi, 
+              "mid":numerics.rnd(query.mid(num)), 
+              "div":numerics.rnd(query.div(num))})
 
 def eg_function_9():
-
- function(   data,l,r)
-  data = DATA.read(the.file)
-  local left,right,A,B,c = half(data)
-  print(#left,#right)
-  l,r = DATA.clone(data,left), DATA.clone(data,right)
-  print("l",o(stats(l)))
-  print("r",o(stats(r))) end)
+  data = Data.Data(d.the.get("file"))
+  left,right,A,B,c = data.half()
+  print(len(left),len(right))
+  l,r = data.clone(left), data.clone(right)
+  print("l",strings.o(l.stats()))
+  print("r",strings.o(r.stats()))
 
 def eg_function_10():
-
- function(   data,l,r)
-  showTree(tree(DATA.read(the.file))) end)
+  showTree(tree(Data.Data(d.the.file)))
 
 def eg_function_11():
-
- function(    data,best,rest)
-  data = DATA.read(the.file)
-  best,rest = sway(data)
-  print("\nall ", o(stats(data)))
-  print("    ",   o(stats(data,div)))
-  print("\nbest", o(stats(best)))
-  print("    ",   o(stats(best,div)))
-  print("\nrest", o(stats(rest)))
-  print("    ",   o(stats(rest,div)))
-  print("\nall ~= best?", o(diffs(best.cols.y, data.cols.y)))
-  print("best ~= rest?", o(diffs(best.cols.y, rest.cols.y))) end)
+  data = Data.Data(d.the["file"])
+  best,rest = data.sway()
+  print("\nall ", strings.o(data.stats()))
+  print("    ",   strings.o(data.stats(query.div)))
+  print("\nbest", strings.o(best.stats()))
+  print("    ",   strings.o(best.stats(query.div)))
+  print("\nrest", strings.o(rest.stats()))
+  print("    ",   strings.o(rest.stats(query.div)))
+  print("\nall ~= best?", strings.o(numerics.diffs(best.cols.y, data.cols.y)))
+  print("best ~= rest?", strings.o(numerics.diffs(best.cols.y, rest.cols.y)))
 
 def eg_function_12():
-
- function(    data,best,rest, b4)
-  data = DATA.read(the.file)
-  best,rest = sway(data)
-  print("all","","","",o{best=#best.rows, rest=#rest.rows})
-  for k,t in pairs(bins(data.cols.x,{best=best.rows, rest=rest.rows})) do
-    for _,range in pairs(t) do
-      if range.txt ~= b4 then print"" end
+  data = Data.Data(d.the["file"])
+  best,rest = data.sway()
+  print("all","","","", strings.o({"best":len(best.rows), "rest":len(rest.rows)}))
+  for k,t in discretization.bins(data.cols.x,{"best":best.rows, "rest":rest.rows}).items():
+    for _,range in enumerate(t):
+      if range.txt != b4:
+            print("")
       b4 = range.txt
       print(range.txt,range.lo,range.hi,
-           rnd(value(range.y.has, #best.rows,#rest.rows,"best")),
-           o(range.y.has)) end end end)
+           numerics.rnd(value(range.y.has, len(best.rows),len(rest.rows),"best")),
+           strings.o(range.y.has))
 
 
 
